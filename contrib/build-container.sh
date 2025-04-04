7e30652e9b44bcde162dd03cd1053c5bdf333931
Anant Sharma <anants@nvidia.com>
2025-04-04 11:42:58 -0400

    contrib: use cuda-dl-base image for docker builds (#115)

--- contrib/build-container.sh
+++ contrib/build-container.sh
@@ -18,9 +18,7 @@
 SOURCE_DIR=$(dirname "$(readlink -f "$0")")
 BUILD_CONTEXT=$(dirname "$(readlink -f "$SOURCE_DIR")")
 DOCKER_FILE="${SOURCE_DIR}/Dockerfile"
-
 commit_id=$(git rev-parse --short HEAD)
-FULLCOMMIT=$(git rev-parse HEAD)
 
 # Get latest TAG and add COMMIT_ID for dev
 latest_tag=$(git describe --tags --abbrev=0 $(git rev-list --tags --max-count=1 main) | sed 's/^v//') || true
@@ -28,16 +26,13 @@ if [[ -z ${latest_tag} ]]; then
     latest_tag="0.0.1"
     echo "No git release tag found, setting to unknown version: ${latest_tag}"
 fi
-
-BASE_IMAGE=nvcr.io/nvidia/pytorch
-BASE_IMAGE_TAG=25.02-py3
 VERSION=v$latest_tag.dev.$commit_id
-UBUNTUOS="24.04"
-USE_LOCAL_DIR=0
-NIXL_DIR=/tmp/nixl
 
-NIXL_COMMIT=ec6345c5279142a3805ab1a0e876f954d079dbf7
-NIXL_REPO=ai-dynamo/nixl.git
+BASE_IMAGE=nvcr.io/nvidia/cuda-dl-base
+BASE_IMAGE_TAG=25.03-cuda12.8-devel-ubuntu24.04
+WHL_PLATFORM=manylinux_2_39_x86_64
+WHL_PYTHON_VERSIONS="3.12"
+OS="ubuntu24"
 
 get_options() {
     while :; do
@@ -47,14 +42,14 @@ get_options() {
             exit
             ;;
         --base-image)
-	    if [ "$2" ]; then
-		BASE_IMAGE="$2"
-		shift
-	    else
-		missing_requirement $1
-	    fi
-	    ;;
-        --base-imge-tag)
+            if [ "$2" ]; then
+                BASE_IMAGE="$2"
+                shift
+            else
+                missing_requirement $1
+            fi
+        ;;
+        --base-image-tag)
             if [ "$2" ]; then
                 BASE_IMAGE_TAG=$2
                 shift
@@ -62,9 +57,9 @@ get_options() {
                 missing_requirement $1
             fi
             ;;
-	--os)
+        --os)
             if [ "$2" ]; then
-                UBUNTUOS=$2
+                OS=$2
                 shift
             else
                 missing_requirement $1
@@ -81,9 +76,14 @@ get_options() {
                 missing_requirement $1
             fi
             ;;
-	--use-local)
-	    USE_LOCAL_DIR=1
-	    ;;
+        --python-versions)
+            if [ "$2" ]; then
+                WHL_PYTHON_VERSIONS=$2
+                shift
+            else
+                missing_requirement $1
+            fi
+            ;;
         --)
             shift
             break
@@ -101,36 +101,34 @@ get_options() {
         shift
     done
 
-    if [[ $UBUNTUOS == "22.04" ]]; then
-	BASE_IMAGE=nvcr.io/nvidia/cuda-dl-base
-	BASE_IMAGE_TAG=24.10-cuda12.6-devel-ubuntu22.04
+    if [[ $OS == "ubuntu22" ]]; then
+        BASE_IMAGE_TAG=24.10-cuda12.6-devel-ubuntu22.04
+        WHL_PLATFORM=manylinux_2_34_x86_64
     fi
 
     if [ -z "$TAG" ]; then
         TAG="--tag nixl:${VERSION}"
     fi
-
-    if [ $USE_LOCAL_DIR -eq 1 ]; then
-	NIXL_DIR=$BUILD_CONTEXT
-	NIXL_COMMIT=$FULLCOMMIT
-    fi
 }
 
 show_build_options() {
     echo ""
-    echo "Building NIXL Image: '${TAG}' for Ubuntu${UBUNTUOS}"
-    echo "Using local nixl source: ${NIXL_DIR}"
-    echo "    Build Context: ${BUILD_CONTEXT}"
+    echo "Building NIXL Image"
+    echo "Image Tag: ${TAG}"
+    echo "Build Context: ${BUILD_CONTEXT}"
+    echo "Base Image: ${BASE_IMAGE}:${BASE_IMAGE_TAG}"
+    echo "Python Versions for wheel build: ${WHL_PYTHON_VERSIONS}"
+    echo "Wheel Platform: ${WHL_PLATFORM}"
 }
 
 show_help() {
-    echo "usage: build.sh"
+    echo "usage: build-container.sh"
     echo "  [--base base image]"
-    echo "  [--base-imge-tag base image tag]"
+    echo "  [--base-image-tag base image tag]"
     echo "  [--no-cache disable docker build cache]"
-    echo "  [--os [24.04|22.04] to select Ubuntu version]"
+    echo "  [--os [ubuntu24|ubuntu22] to select Ubuntu version]"
     echo "  [--tag tag for image]"
-    echo "  [--use-local copy current source dir to container]"
+    echo "  [--python-versions python versions to build for, comma separated]"
     exit 0
 }
 
@@ -145,30 +143,15 @@ error() {
 
 get_options "$@"
 
-show_build_options
-
-if [ $USE_LOCAL_DIR -eq 0 ]; then
-    if [ -d "$NIXL_DIR" ]; then
-	echo "Warning: $NIXL_DIR exists, skipping clone"
-    else
-	git clone https://github.com/${NIXL_REPO} ${NIXL_DIR}
-    fi
-
-    if ! git checkout ${NIXL_COMMIT}; then
-	echo "ERROR: Failed to checkout commit ${NIXL_COMMIT}."
-	echo "Please delete $NIXL_DIR and retry."
-	exit 1
-    fi
-else
-    if [ -d "$NIXL_DIR/build" ]; then
-	echo "Please delete the build directory before creating container"
-	exit 1
-    fi
+if [ -d "$NIXL_DIR/build" ]; then
+    echo "Please delete the build directory before creating container"
+    exit 1
 fi
 
-BUILD_CONTEXT_ARGS+=" --build-context nixl=$NIXL_DIR"
-BUILD_ARGS+=" --build-arg NIXL_COMMIT=${NIXL_COMMIT} --build-arg NIXL_REPO=${NIXL_REPO}"
 BUILD_ARGS+=" --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG"
-BUILD_ARGS+=" --build-arg UBUNTUOS=$UBUNTUOS"
+BUILD_ARGS+=" --build-arg WHL_PYTHON_VERSIONS=$WHL_PYTHON_VERSIONS"
+BUILD_ARGS+=" --build-arg WHL_PLATFORM=$WHL_PLATFORM"
 
-docker build -f $DOCKER_FILE $BUILD_ARGS $TAG $NO_CACHE $BUILD_CONTEXT_ARGS $BUILD_CONTEXT
+show_build_options
+
+docker build -f $DOCKER_FILE $BUILD_ARGS $TAG $NO_CACHE $BUILD_ARGS $BUILD_CONTEXT
