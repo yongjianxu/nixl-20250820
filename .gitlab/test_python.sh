b927d6045ff17f3a34e90e9cc2bbbd1f158c6b22
Mikhail Brinskiy <brminich@users.noreply.github.com>
2025-06-08 17:25:16 +0200

    CI: Enable arm build (#427)
    * CI: Enable arm build
    
    Signed-off-by: Mikhail Brinskii <mikhailb@nvidia.com>
    
    * TEST: Fix pathes for arm
    
    Signed-off-by: Mikhail Brinskii <mikhailb@nvidia.com>
    
    * TEST: Fix pathes in python test for arm
    
    Signed-off-by: Mikhail Brinskii <mikhailb@nvidia.com>
    
    ---------
    
    Signed-off-by: Mikhail Brinskii <mikhailb@nvidia.com>

--- .gitlab/test_python.sh
+++ .gitlab/test_python.sh
@@ -27,13 +27,16 @@ fi
 
 apt-get -qq install liburing-dev
 
-export LD_LIBRARY_PATH=${INSTALL_DIR}/lib:${INSTALL_DIR}/lib/x86_64-linux-gnu:${INSTALL_DIR}/lib/x86_64-linux-gnu/plugins:/usr/local/lib:$LD_LIBRARY_PATH
+ARCH=$(uname -m)
+[ "$ARCH" = "arm64" ] && ARCH="aarch64"
+
+export LD_LIBRARY_PATH=${INSTALL_DIR}/lib:${INSTALL_DIR}/lib/$ARCH-linux-gnu:${INSTALL_DIR}/lib/$ARCH-linux-gnu/plugins:/usr/local/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/lib64/stubs:/usr/local/cuda/lib64:/usr/local/cuda-12.8/compat:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/compat/lib.real:$LD_LIBRARY_PATH
 export CPATH=${INSTALL_DIR}/include:$CPATH
 export PATH=${INSTALL_DIR}/bin:$PATH
 export PKG_CONFIG_PATH=${INSTALL_DIR}/lib/pkgconfig:$PKG_CONFIG_PATH
-export NIXL_PLUGIN_DIR=${INSTALL_DIR}/lib/x86_64-linux-gnu/plugins
+export NIXL_PLUGIN_DIR=${INSTALL_DIR}/lib/$ARCH-linux-gnu/plugins
 
 pip3 install --break-system-packages .
 pip3 install --break-system-packages pytest
